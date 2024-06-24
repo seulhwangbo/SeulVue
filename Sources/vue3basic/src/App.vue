5d69c15c4accd4891bdb14afa5ddfdc0dd7d1e73
seulhwangbo <tmf589@naver.com>
2024-06-24 17:13:37 +0900

    0624

---
+++ Sources/vue3basic/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p>book.author: {{ book.author }}</p>
+    <p>book.title :{{ book.title }}</p>
+    <p>author : {{ author }}</p>
+    <p>title : {{ title }}</p>
+  </div>
+</template>
+
+<script>
+import { reactive } from 'vue'
+
+export default {
+  setup() {
+    const book = reactive({
+      author: 'Vue3',
+      year: '2024',
+      title: 'Vue3 기본',
+      description: '당신을 이 책을 읽습니다 ;)',
+      price: '22000'
+    })
+
+    return {
+      book
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ Sources/vue3basic/src/backup/01/AppVue3_01.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button v-on:click="increment">Counter vue3: {{ counter }}</button>
+  </div>
+</template>
+
+<script>
+import { onMounted, ref } from 'vue'
+
+export default {
+  setup() {
+    const counter = ref(0)
+    const increment = () => counter.value++
+
+    onMounted(() => {
+      console.log('컴포넌트가 마운트 되었음!')
+    })
+    return {
+      counter,
+      increment
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
